7776d52824756f60b84d59f4532e37e987370aa7
addyp1911 <53856899+addyp1911@users.noreply.github.com>
2019-09-02 23:48:52 +0530

    Update OrderedLinkedList.py

--- python/DataStructures/OrderedLinkedList/OrderedLinkedList.py
+++ python/DataStructures/OrderedLinkedList/OrderedLinkedList.py
@@ -6,12 +6,10 @@ import OrderedLinkedListBL as ol
 ll=ol.LinkedList()
 file1=open(r"/home/admin1/Desktop/sample.txt",'r')
 file_content=file1.read().split()
-list1=[]
 for i in file_content:
-    list1.append(int(i))
     ll.addFirst(i)
     ll.printList()
-print("the sorted list is ",ll.sort(list1))
+print("the sorted list is ",ll.sort())
 
 
 
