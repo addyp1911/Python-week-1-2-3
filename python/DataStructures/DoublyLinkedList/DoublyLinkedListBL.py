fd2d0e238acdcccbc32ab8f46cb832cc79eda499
addyp1911 <53856899+addyp1911@users.noreply.github.com>
2019-09-02 20:39:39 +0530

    Update DoublyLinkedListBL.py

--- python/DataStructures/DoublyLinkedList/DoublyLinkedListBL.py
+++ python/DataStructures/DoublyLinkedList/DoublyLinkedListBL.py
@@ -1,4 +1,4 @@
-import string
+
 class Node:
     def __init__(self,data):
         self.data=data
@@ -7,7 +7,7 @@ class Node:
 class DoublyLinkedList:
     def __init__(self):
         self.head=None
-        self.tail=None
+      
     def insertfirst(self,data):
         new_node=Node(data)
         new_node.prev=None
@@ -15,9 +15,9 @@ class DoublyLinkedList:
         if(self.head is None):
             self.head=new_node
             return
-       
         self.head.prev=new_node
-        self.head=new_node 
+        self.head=new_node
+        
     def insertlast(self,data):
         new_node=Node(data)
         new_node.next=None
@@ -27,9 +27,16 @@ class DoublyLinkedList:
         temp=self.head
         while(temp.next is not None):
             temp=temp.next
-
         temp.next=new_node
         new_node.prev=temp.next
+        
+    def removefirst(self):
+        if(self.head is None):
+            print("the list is empty")
+            return
+        self.head=self.head.next
+        self.head.prev=None
+        
     def removelast(self):
         temp=self.head
         prev=None
@@ -38,17 +45,17 @@ class DoublyLinkedList:
             temp=temp.next
         prev.next=temp.next
         temp.next=None
+        
     def addafternode(self,prev_node,data):
         new_node=Node(data)
         if(prev_node is None):
             print("the previous node cant be none")
             return 
-        else:
-            new_node.next=prev_node.next
-            prev_node.next=new_node
-            new_node.prev=prev_node
-            if(new_node.next is not None):
-                new_node.next.prev=new_node
+        new_node.next=prev_node.next
+        prev_node.next=new_node
+        new_node.prev=prev_node
+        if(new_node.next is not None):
+            new_node.next.prev=new_node
 
     def printlist(self):
         temp = self.head
