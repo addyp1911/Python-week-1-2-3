cc4996c4eb8d5b446e574d0de86cafd968fccd77
addyp1911 <53856899+addyp1911@users.noreply.github.com>
2019-09-10 23:41:41 +0530

    Update StockAccountListBL.py

--- python/Oops/StockAccountList/StockAccountListBL.py
+++ python/Oops/StockAccountList/StockAccountListBL.py
@@ -3,24 +3,20 @@
 # date : 29/08/2019
 # method to print the stock account details of company using linked list
 
-# my json file stockrecord
-# {"shareholder1":{"name":"sonal k","stock":"HCL","shareprice":"2000","share_num":"30"},
-#  "shareholder2":{"name":"jay s","stock":"E&Y","shareprice":"3000","share_num":"50"},
-#  "shareholder3":{"name":"lata s","stock":"H&M","shareprice":"1500","share_num":"10"}
-# }
-
 #defining a node class
-class Node:
+import json
+class  Node:
     def __init__(self,data):
         self.data=data
         self.next=None
-
-#implementing a linked list with keys as nodes(keys are the record number as defined in the json file)       
-class StockAccount:
+        
+class StockAccountList:
     def __init__(self):
-        self.head=None
+        with open("stock.json",'r') as f:
+            self.stock_file=json.load(f)
+        self.head=None 
+        self.size=0   
 
-#method to add the keys of the stockfile as nodes to the linked list
     def add_node(self,data):
         new_node=Node(data)
         if(self.head is None):
@@ -28,32 +24,38 @@ class StockAccount:
             return
         new_node.next=self.head
         self.head=new_node
+        self.size+=1
 
-#method to delete the specific node as entered by the user , using the key
-    
-    def delete_node(self,data):
-        t=self.head
-        while(t.next.next!=None):
-            if(t==None):
-                return
-            elif(t!=None and t.data==data):
+    def delete_node(self,name):
+        temp=self.head
+        prev=None
+        while(temp):
+            if(temp.data["name"]==name and temp==self.head):
                 self.head=self.head.next
+            elif(temp.data["name"]!=name and temp!=None):
+                prev=temp
+            elif(temp.data["name"]==name and temp!=None):
+                prev.next=temp.next
             else:
-                while(t!=None and t.data!=data):
-                    prev=t
-                    t=t.next  
-                prev.next=t.next
-            t=t.next 
-#new dictionary defined to modify the existing stock file after deletion of a particular record in it 
-    
+                print("the name not found in the stock file!")
+                return
+            temp=temp.next 
+        self.size-=1            
+
+    def print_stock_file(self):
+        temp=self.head
+        while(temp):
+            print(temp.data)
+            temp=temp.next
+
+    def update_stock_file(self,index,key):
+        temp=self.head
+        count=0
+        while(temp):
+            if(count==index):
+                if(temp.data[key] is not None):
+                    new_value=input("enter the new details= ")
+                    temp.data[key]=new_value
+            temp=temp.next
+            count+=1
   
-
-#method to check whether a particular record is present in the stock file
-    def check_key(self,key,file):
-        for k,v in file.items():
-            if(k==key):
-                return True      
-        return False
-
-#creating the object        
-ll=StockAccount()    
