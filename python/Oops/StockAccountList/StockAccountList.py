e95a4114e2802f5d811fd0fbd4eef0f0922269c0
addyp1911 <53856899+addyp1911@users.noreply.github.com>
2019-09-10 23:43:17 +0530

    Update StockAccountList.py

--- python/Oops/StockAccountList/StockAccountList.py
+++ python/Oops/StockAccountList/StockAccountList.py
@@ -6,37 +6,39 @@
 # be added or removed easily. Do not use any Collection Library to implement Linked
 # List
 
-import StockAccountListBL as cd
-import json
-with open("/home/admin1/Desktop/commerciallist.json",'r') as f:
-    stock_file=json.load(f)
+import StockAccountListBL as sb
+    sl=sb.StockAccountList()
+    for records in sl.stock_file["stockaccount"]:
+            sl.add_node(records)
 
-#adding keys to linked list
-for i in stock_file:
-    cd.ll.add_node(i)
+    while(True):
+        i=int(input("1.enter 1 to add a new person to stock file\n2.enter 2 to delete a person from the stockfile\n3.enter 3 to print the stockfile\n4.enter 4 to update the stockfile\n5.enter 5 to exit the application\n"))     
+        if i==1:
+            new_dict={}
+            name=input("enter the name of the person =")
+            stock=input("enter the stock symbol= ")
+            shares=input("enter the shares= ")
+            price=input("enter the share price= ")
+            new_dict.update({"name":name,"stock":stock,"share":shares,"share_price":price})
+            sl.add_node(new_dict) 
 
-cd.ll.printkey()
+        elif i==2:
+            name=input("enter the name of the person whose record you would delete= ")
+            sl.delete_node(name)
 
-#asking for user to update any customer detail
-update_key=input("enter the record key you would like to update?= ")
+        elif i==3:
+            sl.print_stock_file() 
 
-if(cd.ll.check_key(update_key,stock_file)):
-      update_value=input("enter the record attribute you would like to update?= ")
-      print(cd.ll.update(update_key,update_value,stock_file))
-# new record being added to the stock file if the key is not present in it
-else:
-        name=input("enter the shareholder name= ")
-        stock=input("enter the stockname he wants to buy= ")
-        price=input("enter the price of the stock= ")
-        share_num=input("enter the number of shares he wants to buy= ")
-        key=input("enter the key of this record= ")
-        add_dict={key:{"name":name,"stock":stock,"price":price,"share_num":share_num}}
-        stock_file.update(add_dict)
-        print(stock_file)      
-#asking the user if he would delete any record from the existing file
-user_key=input("enter the node you would like to delete?= ")
-if(cd.ll.check_key(user_key,stock_file)): 
-    cd.ll.delete_node(user_key)
-    #the modified stockfile is printed
-    print("the modified stockfile is= ",cd.ll.new_dict(stock_file))  
+        elif i==4:
+            index=int(input("enter the index of the record= "))
+            if(index > sl.size or index < 0):
+                print("invalid index!")
+            else:    
+                key=input("enter the attribute that needs to be updated= ")
+                sl.update_stock_file(index,key)
+
+        elif i==5:
+            exit()
+            break
+                  
   
